f3d2cea1e44729a30815df9029aaf245306ddafe
bassfridge <39544365+bassfridge@users.noreply.github.com>
2019-11-16 16:04:32 -0600

    Add files via upload

--- gui.py
+++ gui.py
@@ -4,12 +4,35 @@ import tkinter.colorchooser
 from tkinter import *
 import sys
 
-#default color
-color = "pink"
 
 #methods
+#update brush size from slider
+def updateSize(val):
+    global selectedWidth
+    selectedWidth = w1.get()
+
+#method to start painting
+def startPaint(e):
+    global lastX, lastY
+    canvas.bind('<B1-Motion>', paint)
+    lastX = e.x
+    lastY = e.y
+
+
+#method to perform painting
+def paint(e):
+    global lastX, lastY, selectedWidth, color
+    updateSize(None)
+    x = e.x
+    y = e.y
+    canvas.create_line((lastX, lastY, x, y), width = selectedWidth, fill = color)
+    lastX = x
+    lastY = y
+
+
 #method to select color
 def newColor():
+    global color
     color = colorchooser.askcolor()
     colorCanvas.config(bg = color[1])
 
@@ -33,7 +56,8 @@ def leaveChat():
 gui = tk.Tk()
 gui.title('PictoChat The Rebirth')
 gui.minsize(1200, 610)
-
+#default color
+color = "pink"
 
 #populate our gui
 #canvas object
@@ -55,12 +79,26 @@ button4.place(x = 980, y = 325)
 #selected color display label
 label1 = Label(gui, text = "Current Color")
 label1.place(x = 1040, y= 240)
+#label for brush size
+label2 = Label(gui, text = "Brush Size")
+label2.place(x = 1050, y = 100)
 #selected color display viewable color
 colorCanvas = Canvas(gui, bg = color, width = 203, height = 50)
 colorCanvas.place(x = 980, y = 260)
+#slider for size
+w1 = Scale(gui, from_ = 1, to_ = 50, length = 200, orient = HORIZONTAL, command = updateSize)
+w1.set(5)
+w1.place(x = 980, y= 115)
 
 #if we have messages run this line
 flashColor(button3, 0)
+
+#data for drawing
+lastX, lastY = None,None
+canvas.bind('<1>', startPaint)
+#size
+selectedWidth = w1.get()
+
 
 
 #run our gui
